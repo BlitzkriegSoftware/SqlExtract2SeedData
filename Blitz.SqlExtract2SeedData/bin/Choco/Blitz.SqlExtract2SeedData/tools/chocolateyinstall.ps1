d485828c95c6f116dccc4f58ecacdc56c70a6ce8
Stuart Williams <spookdejur@hotmail.com>
2020-12-28 13:55:39 -0800

    Choco Config Files

---
+++ Blitz.SqlExtract2SeedData/bin/Choco/Blitz.SqlExtract2SeedData/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'blitz.sqlextract2seeddata'
+$url = 'https://github.com/BlitzkriegSoftware/SqlExtract2SeedData/releases/download/v1.1.2/BlitzSqlExtract2SeedData.exe'
+$checksum="24EE4CCB81C71AF4C52C1882A203BA831971F330E192B4410C5983FC2445A3C9"
+$fileType = 'exe'
+$silentArgs = '/SILENT'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes -checksum $checksum -checksumType "sha256"
